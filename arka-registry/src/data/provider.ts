3d0f011a7ca746b3c9a7047a41cb5feb874a9441
aleem1314 <aleem@vitwit.com>
2024-12-31 14:42:01 +0530

    chore: update providers

--- arka-registry/src/data/provider.ts
+++ arka-registry/src/data/provider.ts
@@ -4,11 +4,11 @@ import { Provider } from '../types/provider';
 const providers: Provider[] = [
     {
         display_name: "Akash",
-        name: "akash"
+        name: "AKASH_CHAT_API"
     },
     {
         display_name: "OpenAI",
-        name: "open-ai"
+        name: "OPENAI"
     },
 ]
 export default providers;
